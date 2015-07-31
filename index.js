3b874674a44ff930a7f716a6f03beec31506c2aa
James Muerle <jmuerle@greek-juniper.local>
2015-07-31 00:32:18 -0700

    Changing the dependency again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var caseRequester = require("/fogbugzCaseRequester.js");
+var caseRequester = require("./fogbugzCaseRequester");
 
 app.set('port', (process.env.PORT || 5000));
 
