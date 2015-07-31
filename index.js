035ab253847439a4029abfdb953aaa55848a4f03
James Muerle <jmuerle@greek-juniper.local>
2015-07-31 02:13:15 -0700

    Cleaning up logs and comments

--- index.js
+++ index.js
@@ -51,20 +51,17 @@ app.get('/fogbugzUpdate', function(request, response) {
 });
 
 function addCaseObjToDb(client, caseObj, callback) {
+  console.log("Inserting new case into db with number:" + caseObj.caseNumber);
   client.query(
     'INSERT INTO cases (case_number, creation_date) VALUES (' + caseObj.caseNumber + ', \'' + caseObj.dateOpened + '\')' +
     ' RETURNING *',
     function (err, result) {
-      console.log("got result:");
-      console.log(result.rows);
       callback(result.rows[0]);
-      // client.query('SELECT * FROM cases WHERE case_number=' + caseObj.caseNumber, function (err, result) {
-      //   callback(result.rows[0]);
-      // });
     }
   );
 }
 
+// The following are sample fields of a caseArgs object.
 // assignedToName: 'Kevin O\'Connor',
 // caseNumber: '74770',
 // eventText: '<p>\r\n\tre-opening</p>\r\n',
@@ -75,6 +72,8 @@ function addCaseObjToDb(client, caseObj, callback) {
 // statusName: 'Active',
 // title: 'Test Bug for Fangtastic Four'
 function addUpdateToDb(client, caseObj, caseArgs, callback) {
+  console.log("Inserting new update into db with args:");
+  console.log(caseArgs);
   var eventText = caseArgs.eventText || null,
     assignedToName = caseArgs.assignedToName || null,
     query =
@@ -83,69 +82,14 @@ function addUpdateToDb(client, caseObj, caseArgs, callback) {
       "VALUES (" + caseObj.case_id + ", '" + escapeQuotes(caseArgs.personEditingName) + "', '" + escapeQuotes(assignedToName) +
         "', '" + caseArgs.eventType + "', '" + caseArgs.statusName + "', '" + escapeQuotes(eventText) + "', '" + caseArgs.eventTime +
         "', '" + escapeQuotes(caseArgs.title) + "', '" + escapeQuotes(caseArgs.projectName) + "')";
-  console.log("going to execute query: " + query);
   client.query(query, function (err) {
-    console.log("Error:");
-    console.log(err);
+    callback();
   });
-    // switch (caseArgs.eventType) {
-    //   case "CaseEdited":
-    //     handleCaseEdited(client, caseArgs);
-    //     break;
-    //   case "CaseAssigned":
-    //     handleCaseAssigned(client, caseArgs);
-    //     break;
-    //   case "CaseResolved":
-    //     handleCaseResolved(client, caseArgs);
-    //     break;
-    //   case "CaseClosed":
-    //     handleCaseClosed(client, caseArgs);
-    //     break;
-    //   case "CaseReopened":
-    //     handleCaseReopened(client, caseArgs);
-    //     break;
-    //   case "CaseReactivated":
-    //     handleCaseReactivated(client, caseArgs);
-    //     break;
-    // }
 }
 
 function escapeQuotes(str) {
   return str.replace(/'/g, "''");
 }
-
-// function handleCaseEdited(client, caseArgs) {
-//   console.log("case edited");
-//   console.log(caseArgs);
-// }
-
-// function handleCaseAssigned(client, caseArgs) {
-//   console.log("case assigned");
-//   console.log(caseArgs);
-// }
-
-// function handleCaseResolved(client, caseArgs) {
-//   console.log("case resolved");
-//   console.log(caseArgs);
-
-// }
-
-// function handleCaseClosed(client, caseArgs) {
-//   console.log("case closed");
-//   console.log(caseArgs);
-
-// }
-
-// function handleCaseReopened(client, caseArgs) {
-//   console.log("case reopened");
-//   console.log(caseArgs);
-
-// }
-
-// function handleCaseReactivated(client, caseArgs) {
-//   console.log("case reactivated");
-//   console.log(caseArgs);
-// }
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
